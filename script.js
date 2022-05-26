bb444e6cac354b673f66467a4dd7875ecba3fb5f
Riddy105 <99593252+Riddy105@users.noreply.github.com>
2022-05-26 18:09:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,17 +2,21 @@
 let billValue;
 let tip;
 let peopleValue;
-let individualTip;
 let tipPerHead;
 let totalPerHead;
-let customValue = document.querySelector(`.custom-input`).value;
+let customValue;
+let customTip;
 let buttonValues = document.querySelectorAll(`.tip-btn`);
 let warningMessage = document.querySelector(`.display-cont-para`);
 let tipValue = document.querySelector(`.tip-value`);
 let totalValue = document.querySelector(`.total-value`);
+let resetEl = document.querySelector(`.reset`);
+
+// AN ARRAY STORING ALL THE VALUES FOR THE 5 TIP BUTTONS.
+// I THOUGHT IT'LL BE BEST TO LOOP OVER THEM AS I LOOP OVER THE CORRESPONDING BUTTONS AS WELL 😉
 const tipButtons = [5, 10, 15, 25, 50];
 
-// THREE FUNCTIONS USED TO CALCULATE TIP, INDIVIDUAL TIP AND INDIVIDUAL TOTAL
+// THREE FUNCTIONS USED TO CALCULATE TIP, TIP/HEAD AND TOTAL/HEAD
 let calcTip = function (percent, bill) {
   return (percent / 100) * bill;
 };
@@ -23,31 +27,50 @@ let calcTotalPerHead = function (bill, tip, people) {
   return (bill + tip) / people;
 };
 
-// console.log(billValue);
+// RESET FUNCTION
+let reset = function () {
+  billValue = ``;
+  peopleValue = ``;
+  customValue = ``;
+};
 
+// A FOR LOOP WHICH USED TO ITERATE OVER MY BUTTONS(BUTTONVALUES) SINCE THEY ARE STORED IN AN ARRAY-LIKE FORMAT
 for (let i = 0; i < buttonValues.length; i++) {
   buttonValues[i].addEventListener(`click`, function () {
     billValue = Number(document.querySelector(`.bill-input`).value);
+    // THIS LOOPS ALSO HELPS ME ITERATE OVER THE CORRESPONDING (TIPBUTTONS) VALUES I STORED IN AN ARRAY EARLIER.
+    // THEY ARE PASSED AS ARGUMENT TO A FUNCTION THAT CALCULATES THE TIP
     tip = calcTip(tipButtons[i], billValue);
     console.log(tip);
   });
 }
 
 document.addEventListener(`keydown`, function (e) {
-  // console.log(e);
+  // TRYING TO CALCULATE CUSTOM TIP
+  billValue = Number(document.querySelector(`.bill-input`).value);
+  customValue = Number(document.querySelector(`.custom-input`).value);
+  if (billValue && e.key == `Enter` && customValue != 0) {
+    tip = calcTip(customValue, billValue);
+  }
+
   peopleValue = Number(document.querySelector(`.people-input`).value);
   if (e.key == `Enter` && billValue && tip && peopleValue != 0) {
     tipPerHead = calcTipPerHead(peopleValue, tip);
     tipValue.textContent = tipPerHead.toFixed(2);
-    // console.log(tipPerHead);
     totalPerHead = calcTotalPerHead(billValue, tip, peopleValue);
-    // console.log(totalPerHead);
     totalValue.textContent = totalPerHead.toFixed(2);
   }
-  if (peopleValue === 0 && tip) {
+  if (peopleValue == 0 && tip) {
     warningMessage.classList.add(`display`);
   }
   if (peopleValue > 0 && tip) {
     warningMessage.classList.remove(`display`);
   }
+});
+
+resetEl.addEventListener(`click`, function () {
+  console.log(billValue);
+  billValue = 0;
+  peopleValue = ``;
+  customValue = ``;
 });
